883bea6600a6d09700752c5f35da4c73bc780847
LeeJungBum <jungbum05114@gmail.com>
2023-09-01 15:27:13 +0900

    feat: toDecimal 구현

--- src/main/java/org/nhnacademy/homework/Converter/Converter.java
+++ src/main/java/org/nhnacademy/homework/Converter/Converter.java
@@ -11,10 +11,48 @@ public class Converter {
 
     }
 
-    public static int toDecimal(String value, int numeralSystem) {
+    public static long toDecimal(String value, int numeralSystem) {
+        long answer = 0;
         stack.clear();
-//        stackAdd(value);
-        return 0;
+        if (!numeralSystemCheck(value, numeralSystem)) {
+            throw new IllegalArgumentException("올바르지 않은 값이 들어왔습니다");
+        }
+        stackAdd(value);
+        int i = 1;
+        while (!stack.isEmpty()) {
+            char c = stack.pop();
+            int minus = base(c);
+            if (i == 1) {
+                if ((int) c - minus == 0) {
+                    i++;
+                    continue;
+                }
+                answer += (c - minus);
+//                System.out.println("answer = "+answer);
+                i++;
+                continue;
+            }
+            if ((int) c - minus == 0) {
+                i++;
+                continue;
+            }
+            answer += ((int) Math.pow(numeralSystem, i - 1) * (c - minus));
+//            System.out.println("answer = "+answer);
+
+            i++;
+
+        }
+        return answer;
+    }
+
+    private static int base(Character c) {
+        System.out.println((int) c);
+        if ((int) c <= 57) {
+            return 48;
+        } else if ((int) c <= 70) return 55;
+        else {
+            return 87;
+        }
     }
 
     //    public String toBinary()
@@ -40,7 +78,7 @@ public class Converter {
 //    public void hexadecimalToOctal() {
 //
 //    }
-    private static void stackAdd(String value, int numeralSystem) {
+    private static void stackAdd(String value) {
         for (char a : value.toCharArray()) {
             stack.add(a);
         }
@@ -48,18 +86,15 @@ public class Converter {
 
     public static boolean numeralSystemCheck(String value, int numeralSystem) {
         System.out.println(numeralSystem);
-        if (numeralSystem == 2)
-            return binaryCheck(value);
-        else if (numeralSystem == 8)
-            return octalCheck(value);
-        else if (numeralSystem == 16)
-            return hexadecimalCheck(value);
+        if (numeralSystem == 2) return binaryCheck(value);
+        else if (numeralSystem == 8) return octalCheck(value);
+        else if (numeralSystem == 16) return hexadecimalCheck(value);
         return false;
     }
 
     private static boolean hexadecimalCheck(String value) {
         System.out.println("16");
-        return Pattern.matches("^[0-9]+|[a-f]+|[A-F]+$", value);
+        return Pattern.matches("^([0-9]*|[a-f]*|[A-F]*)+$", value);
     }
 
     private static boolean octalCheck(String value) {
