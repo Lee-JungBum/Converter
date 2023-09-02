caabb7e071d517bb945f0d962239fce5a4bee756
LeeJungBum <jungbum05114@gmail.com>
2023-09-03 03:35:26 +0900

    test : 10진수 -> 2진수,8진수 16진수

--- src/test/java/org/nhnacademy/homework/Converter/toNumeral.java
+++ src/test/java/org/nhnacademy/homework/Converter/toNumeral.java
@@ -1,9 +1,26 @@
 package org.nhnacademy.homework.Converter;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class toBinary {
+public class toNumeral {
     @Test
-    @DisplayName("toNumeral")
+    @DisplayName("toNumeral(toHex)")
+    void toNumeralTest1()
+    {
+        Assertions.assertEquals("AAAA",Converter.toNumeral(43690,16));
+    }
+    @Test
+    @DisplayName("toNumeral(toOct)")
+    void toNumeralTest2()
+    {
+        Assertions.assertEquals("7170",Converter.toNumeral(3704,8));
+    }
+    @Test
+    @DisplayName("toNumeral(toBinary)")
+    void toNumeralTest3()
+    {
+        Assertions.assertEquals("10101011",Converter.toNumeral(171,2));
+    }
 }
