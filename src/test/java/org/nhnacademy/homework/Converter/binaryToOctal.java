2c3156575da15f5c8aacccad95002082802d84fc
LeeJungBum <jungbum05114@gmail.com>
2023-09-03 23:59:00 +0900

    test: binaryToOctal 테스트코드 작성

--- src/test/java/org/nhnacademy/homework/Converter/binaryToOctal.java
+++ src/test/java/org/nhnacademy/homework/Converter/binaryToOctal.java
@@ -1,2 +1,23 @@
-package org.nhnacademy.homework.Converter;public class binaryToOctal {
+package org.nhnacademy.homework.Converter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class binaryToOctal {
+    @Test
+    @DisplayName("binaryToOctal 1111")
+    void binaryToOctal1()
+    {
+
+        Assertions.assertEquals("17",Converter.binaryToOctal("1111"));
+    }
+
+    @Test
+    @DisplayName("binaryToOctal 01010")
+    void binaryToOctal2()
+    {
+
+        Assertions.assertEquals("12",Converter.binaryToOctal("01010"));
+    }
 }
